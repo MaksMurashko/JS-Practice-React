7468abdc8e0e40ae3cec99741bc2948230e5f09e
MaksMurashko <maksmd251203@gmail.com>
2024-06-30 18:08:46 +0300

    Some changes...

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import PhotoList from "./PhotoList";
 import Counter from "./Counter";
 import Form from "./Form";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div id="taskBox">
       <PhotoList />
